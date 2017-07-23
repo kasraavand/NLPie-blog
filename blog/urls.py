025f2dc259949309dc4dd49106e7f673d42d8816
kasramvd <kasraavand@gmail.com>
2017-07-23 22:55:11 +0430

    add posts_with_tag url

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<tag>[a-zA-Z-_]+)/$', views.posts_with_tag, name='posts_with_tag'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
