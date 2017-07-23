02e7e4ff800dbf9ad3d8e2ec8e275452df505743
kasramvd <kasraavand@gmail.com>
2017-07-23 22:03:15 +0430

    fix bug by saving the post author and densities before tag for refusing null constraint error

--- blog/forms.py
+++ blog/forms.py
@@ -10,21 +10,23 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text',)
         # widgets = {'tags': forms.TextInput()}
     
-    def save(self, commit=True):
+    def save(self, author, commit=True):
         post_instance = super(PostForm, self).save(commit=commit)
+        # for refusing of null constraint
+        post_instance.author = author
         tags = self.cleaned_data.get('tags', None)
         if tags is not None:
             all_tag_names = re.findall(r'([\w-]+):(\d+)', tags)
+            # for refusing of null constraint 
+            post_instance.tag_densities = all_tag_names
             for t_name, density in all_tag_names:
                 try:
                     t_obj = Tag.objects.get(name=t_name)
                 except Tag.DoesNotExist as exc:
-                    print(exc)
                     t_obj = Tag(name=t_name)
                     t_obj.save()
-                self.save_m2m()
+                post_instance.save()
                 post_instance.tags.add(t_obj)
-            post_instance.tag_densities = all_tag_names
         post_instance.save()
         return post_instance
 
