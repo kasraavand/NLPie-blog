0a205ccc2861fe72fd333522adb096d8edad468b
kasramvd <kasraavand@gmail.com>
2017-08-06 19:25:38 +0430

    add email to comment form

--- blog/models.py
+++ blog/models.py
@@ -37,6 +37,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
+    email = models.EmailField(max_length=50)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
