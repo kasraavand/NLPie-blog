d9f7c12b73bdca0087cc8255ac08079a64d32330
kasramvd <kasraavand@gmail.com>
2017-08-07 12:54:32 +0430

    create a custom truncater

--- blog/templatetags/filters.py
+++ blog/templatetags/filters.py
@@ -1,9 +1,15 @@
 from django import template
 register = template.Library()
 from markdown2 import markdown
+import re
 
 
 @register.filter
 def render(content):
     result = markdown(content, extras=["code-color"] ,safe_mode=False)
     return result
+
+
+@register.filter
+def strip_code(content):
+    return re.sub('<code>\s*|(?:\s*readmore\s*)</code>', '', content)
