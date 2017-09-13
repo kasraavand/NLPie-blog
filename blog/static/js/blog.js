4bd753e2aa18ced4929d8d65118e986ef6fff89f
kasramvd <kasraavand@gmail.com>
2017-09-13 17:03:43 +0430

    add subscribe function

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -1,39 +1,19 @@
-;( function( window, document, undefined )
-{
-    'use strict';
+function subscribe() {
+    var email = document.getElementById('newsletter').value;
+    $.ajax({
+        dataType: "json",
+        url : "subscribe", // the endpoint
+        type : "POST", // http method
+        data : { 'email' : email}, // data sent with the post request
 
-    var form        = '.newsletter',
-        className   = 'newsletter--active',
-        email       = 'input[type="email"]',
-
-        addEventListener = function( element, event, handler )
-        {
-            element.addEventListener ? element.addEventListener( event, handler ) : element.attachEvent( 'on' + event, function(){ handler.call( element ); });
+        // handle a successful response
+        success : function(response) {
+            alert("Thanks for subscription.");
         },
-        forEach = function( elements, fn )
-        {
-            for( var i = 0; i < elements.length; i++ ) fn( elements[ i ], i );
-        },
-        addClass = function( element, className )
-        {
-            element.classList ? element.classList.add( className ) : element.className += ' ' + className;
-        },
-        removeClass = function( element, className )
-        {
-            element.classList ? element.classList.remove( className ) : element.className += element.className.replace( new RegExp( '(^|\\b)' + className.split( ' ' ).join( '|' ) + '(\\b|$)', 'gi' ), ' ' );
-        };
 
-    forEach( document.querySelectorAll( form ), function( $form )
-    {
-        var $email = $form.querySelectorAll( email );
-
-        if( $email.length )
-        {
-            $email = $email[ 0 ];
-            addEventListener( $email, 'keyup', function()
-            {
-                $email.value != '' && /^([\w-\.]+@([\w-]+\.)+[\w-]{2,12})?$/.test( $email.value ) ? addClass( $form, className ) : removeClass( $form, className );
-            });
-        }
-    });
-})( window, document );
+        // handle a non-successful response
+        error : function(xhr, errmsg, err) { 
+            console.log(xhr.status + ": " + xhr.responseText);
+    }
+});
+};
