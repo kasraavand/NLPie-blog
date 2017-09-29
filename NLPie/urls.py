471b4902cee903d01eec9642b7bae81064c4940b
kasramvd <kasraavand@gmail.com>
2017-09-29 21:14:35 +0330

    add logout link

--- NLPie/urls.py
+++ NLPie/urls.py
@@ -22,6 +22,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
-    # url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
