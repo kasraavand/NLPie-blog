48e186ddb0d7f4a7efedd7f4b3b944f53bb6122c
kasramvd <kasraavand@gmail.com>
2017-10-11 13:19:27 +0330

    register subscribe model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Tag, PostView
+from .models import Post, Comment, Tag, PostView, Subscribers
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(PostView)
+admin.site.register(Subscribers)
