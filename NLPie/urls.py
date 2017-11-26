327c51e961e0e5fd9a97c8984de914e798605cd4
kasramvd <kasraavand@gmail.com>
2017-11-27 00:17:29 +0330

    add media url

--- NLPie/urls.py
+++ NLPie/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views
 
 
@@ -24,4 +25,4 @@ urlpatterns = [
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
